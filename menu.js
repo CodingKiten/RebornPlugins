44b00cf834fdd296dd6194e28592a91f1b993114
CodingKiten <121097932+CodingKiten@users.noreply.github.com>
2023-10-23 18:48:46 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,38 +1,21 @@
-// Keep track of whether the cube is currently visible
 let cubeVisible = false;
 let cubeElement;
 
-// Attach an event listener to the right shift key press
 window.addEventListener("keydown", (event) => {
   if (event.code === "ShiftRight") {
-    // Open the cube if it's not visible, or close it if it's already visible
     if (!cubeVisible) {
-      // Create a cube element
       cubeElement = document.createElement("div");
       cubeElement.style.width = "100px";
       cubeElement.style.height = "100px";
       cubeElement.style.backgroundColor = "red";
-      cubeElement.style.transform = "scale(0)";
-      cubeElement.style.transition = "transform 0.5s";
-
-      // Append the cube to the body
+      cubeElement.style.position = "fixed";
+      cubeElement.style.top = "50%";
+      cubeElement.style.left = "50%";
+      cubeElement.style.transform = "translate(-50%, -50%)";
       document.body.appendChild(cubeElement);
-
-      // Trigger animation to open the cube
-      cubeElement.style.transform = "scale(1)";
-
-      // Set the cube visibility to true
       cubeVisible = true;
     } else {
-      // Trigger animation to close the cube
-      cubeElement.style.transform = "scale(0)";
-
-      // Remove the cube element from the DOM after the animation finishes
-      setTimeout(() => {
-        cubeElement.remove();
-      }, 500);
-
-      // Set the cube visibility to false
+      cubeElement.remove();
       cubeVisible = false;
     }
   }
