70b17f4d412d2e23ca27e6a1469b8034c902c2b5
CodingKiten <121097932+CodingKiten@users.noreply.github.com>
2023-10-23 18:35:56 +0200

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1,1 +1,18 @@
 
+// Attach an event listener to the right shift key press
+window.addEventListener("keydown", (event) => {
+  if (event.code === "ShiftRight") {
+    // Create a cube element
+    const cube = document.createElement("div");
+    cube.style.width = "100px";
+    cube.style.height = "100px";
+    cube.style.backgroundColor = "red";
+    cube.style.position = "absolute";
+    cube.style.top = "50%";
+    cube.style.left = "50%";
+    cube.style.transform = "translate(-50%, -50%)";
+
+    // Append the cube to the body
+    document.body.appendChild(cube);
+  }
+});
