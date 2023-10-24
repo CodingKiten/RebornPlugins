744e89d7f44685f6d6f6718963f8c852771d9a4d
CodingKiten <121097932+CodingKiten@users.noreply.github.com>
2023-10-24 07:28:25 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,11 +11,29 @@ window.addEventListener("keydown", (event) => {
       cubeElement.style.position = "fixed";
       cubeElement.style.top = "50%";
       cubeElement.style.left = "50%";
-      cubeElement.style.transform = "translate(-50%, -50%)";
+      cubeElement.style.transform = "translate(-50%, -50%) scale(0)";
+      cubeElement.style.borderRadius = "10px";
+      cubeElement.style.transition = "transform 0.5s, opacity 0.4s";
+      cubeElement.style.opacity = "0";
+
       document.body.appendChild(cubeElement);
+
+      // Trigger animation to open the cube
+      setTimeout(() => {
+        cubeElement.style.transform = "translate(-50%, -50%) scale(1)";
+        cubeElement.style.opacity = "1";
+      }, 10);
+
       cubeVisible = true;
     } else {
-      cubeElement.remove();
+      cubeElement.style.transform = "translate(-50%, -50%) scale(0)";
+      cubeElement.style.opacity = "0";
+
+      // Remove the cube element from the DOM after the animation finishes
+      setTimeout(() => {
+        cubeElement.remove();
+      }, 500);
+
       cubeVisible = false;
     }
   }
